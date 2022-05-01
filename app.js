f07c97557a5e0af340766a9e355d2a96766ca0c7
Anish Ghimire <51573763+anishghimireofficial@users.noreply.github.com>
2022-05-01 15:45:10 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// this function clear all values
+
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+
+// This function display values
+
+function display(value) {
+  document.getElementById("result").value += value;
+}
+
+// This function evalutes the expression and result
+function calculate() {
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
+}
